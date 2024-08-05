c5b4098e4f8bf3f99095e22198c97a552ef307ac
Yury Zolnikov <yuzolnikov@gmail.com>
2024-08-05 19:32:08 +0300

    Updating readme

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-Бот-ассистент для изучения китайского языка.
+Bot assisting in studying Chinese with Telegram
+
+![Telegram](https://telegram.org/img/t_logo.png)
+
+## Quick start
+
+First, clone this repository:
+
+```bash
+git clone git@github.com:Kolbacyn/ch_learn_bot.git
+```
+
+Second, create and activate virtual environment:
+
+```bash
+python -m venv venv
+source venv/Scripts/activate
+```
+Install dependencies from `requirements.txt`:
+
+```bash
+pip install -r requirements.txt
+```
+
+Create an empty `.env` file: and add bot token to it:
+
+```bash
+BOT_TOKEN=1234567890:ABCDefghijklmnopqrstuvwxyz
+
+```
+
+Run bot:
+
+```bash
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,44 @@
+import asyncio
+import logging
+from os import getenv
+
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import SimpleEventIsolation
+from dotenv import load_dotenv
+
+from routers import router as main_router
+
+logging.basicConfig(level=logging.INFO)
+
+users: dict[int, dict[str, list]] = {}
+
+
+def create_dispatcher():
+    dispatcher = Dispatcher(
+        events_isolation=SimpleEventIsolation()
+    )
+    dispatcher.include_router(main_router)
+    # dispatcher.include_router(quiz_router)
+    # dispatcher.include_router(handlers.router)
+    # scene_registry = SceneRegistry(dispatcher)
+    # ... and then register a scene in the registry
+    # by default, Scene will be mounted to the router that passed to the SceneRegistry,
+    # but you can specify the router explicitly using the `router` argument
+    # scene_registry.add(QuizScene)
+    return dispatcher
+
+
+load_dotenv()
+dp = create_dispatcher()
+
+
+async def main():
+    bot = Bot(token=getenv('BOT_TOKEN'))
+    try:
+        await dp.start_polling(bot)
+    finally:
+        bot.session.close()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
