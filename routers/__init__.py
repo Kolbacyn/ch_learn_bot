8d6f4a04e2e49b9990f5fc44ecf71a80478a5cdc
Yury Zolnikov <yuzolnikov@gmail.com>
2024-08-07 21:10:18 +0300

    Adding settings area to the introducing part of bot

--- routers/__init__.py
+++ routers/__init__.py
@@ -4,12 +4,12 @@ from aiogram import Router
 
 from .quiz_handlers import router as act_router
 from .command_handlers import router as cmd_router
-from .hsk_routers import router as hsk_router
+from .user_settings_routers import router as settings_router
 from .flashcard_handler import router as flashcard_router
 
 router = Router(name=__name__)
 
 router.include_router(cmd_router)
-router.include_router(hsk_router)
+router.include_router(settings_router)
 router.include_router(act_router)
 router.include_router(flashcard_router)
