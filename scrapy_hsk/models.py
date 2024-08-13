57c259594e981af96b22d2f6da02758b49c1a7c2
Yury Zolnikov <yuzolnikov@gmail.com>
2024-08-13 08:52:11 +0300

    Changing base model naming

--- scrapy_hsk/models.py
+++ scrapy_hsk/models.py
@@ -1,12 +1,8 @@
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import declared_attr
+from sqlalchemy.ext.declarative import declarative_base, declared_attr
 
 
-class BaseModel:
-    """Base class for SQLAlchemy models."""
-
-    __abstract__ = True
+class PreBase:
 
     id = Column(Integer, primary_key=True)
 
@@ -18,7 +14,7 @@ class BaseModel:
     id = Column(Integer, primary_key=True)
 
 
-Base = declarative_base(cls=BaseModel)
+Base = declarative_base(cls=PreBase)
 
 
 class Word(Base):
